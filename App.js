8111604e27c89d33cd4d0abc90fa978b3d1423e7
BeNn101 <ribeirobenjamin007@gmail.com>
2024-01-17 15:21:18 +0100

    Add Forcast + style

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { Image } from 'react-native';
 import * as Location from 'expo-location';
+import { ScrollView } from 'react-native';
 
 
 export default function App() {
@@ -28,6 +29,7 @@ export default function App() {
         });
 
         fetchWeather(location.coords.latitude, location.coords.longitude);
+        fetchForecast(location.coords.latitude, location.coords.longitude);
       } catch (error) {
         console.error('Error getting location:', error);
       }
@@ -47,25 +49,40 @@ export default function App() {
       });
   }
 
-  // function fetchForecast(lat, lon) {
-  //   const apiKey = 'ef7d32567dbed321820828d0ba33b91c';
-  //   const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=fr&cnt=40`;
+  function fetchForecast(lat, lon) {
+    const apiKey = 'ef7d32567dbed321820828d0ba33b91c';
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=fr`;
 
-  //   axios.get(forecastUrl)
-  //     .then(function(response) {
-  //       setForecastData(response.data.list);
-  //     })
-  //     .catch(function(error) {
-  //       console.error('Erreur lors de la récupération des données de prévision:', error);
-  //     });
-  // }
+    axios.get(forecastUrl)
+      .then(function(response) {
+        setForecastData(response.data.list);       
+      })
+      .catch(function(error) {
+        console.error('Erreur lors de la récupération des données de prévision:', error);
+      });
+  }
+  
+  function formatForecastDate(dt_txt) {
+    const date = new Date(dt_txt);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+  
+    return {
+      formattedDate: `${day}/${month}`,
+      formattedTime: `${hours}:${minutes}`,
+    };
+  }
 
   return (
     <View style={styles.container}>
+       
       <ImageBackground
         source={require('./img/picture.png')}
         style={styles.backgroundImage}
       >
+        
         <View style={styles.overlay}>
         <Text style={styles.title}>App Météo </Text>
         </View>
@@ -88,7 +105,37 @@ export default function App() {
       <View style={styles.forecastSection}>
         <Text style={styles.forecastTitle}>Prévisions</Text>
       </View>
+
+      {forecastData && (
+  <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+    <View style={styles.forecastCardSection}>
+      {forecastData.map((forecast, index) => {
+        const { formattedDate, formattedTime } = formatForecastDate(forecast.dt_txt);
+
+        return (
+          <View key={index} style={styles.forecastCard}>
+            <Text style={styles.forecastDate}>{formattedDate}</Text>
+            <Text style={styles.forecastTime}>{formattedTime}</Text>
+            <Text style={styles.forecastTemp}>{Math.floor(forecast.main.temp)}°C</Text>
+            <Text style={{ fontSize: 15 , fontWeight : 'bold'}}>
+              {forecast.weather[0].main.charAt(0).toUpperCase() +
+                forecast.weather[0].main.slice(1)}
+            </Text>
+            <Image
+              style={{ width: 50, height: 50 }}
+              source={{
+                uri: `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`,
+              }}
+            />
+          </View>
+        );
+      })}
+    </View>
+  </ScrollView>
+)}
+      
       </ImageBackground>
+      
     </View>
   );
 }
@@ -162,12 +209,41 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  forecastDate: {
+    fontSize : 20,
+    fontWeight : 'bold',
+    marginBottom : 6
+  },
+  forecastTime: {
+    fontSize : 17,
+    marginBottom : 6,
+  },
+  forecastTemp : {
+    fontSize : 23,
+    color : 'grey',
+    marginBottom : 10,
+  },
+  
   backgroundImage: {
     flex: 1,
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
   },
+  forecastCardSection: {
+    flexDirection: 'row',     
+    borderRadius: 8,
+    marginBottom : 30 
+  },
+  forecastCard: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 8,
+    padding: 27,
+    margin: 10,
+    alignItems: 'center',
+    height: 200,  
+  },
+  
 });
 
  
